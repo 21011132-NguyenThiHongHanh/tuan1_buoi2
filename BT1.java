a4a288d876cb7005141232787ad87f470113fe53
21011132-NguyenThiHongHanh <124747121+21011132-NguyenThiHongHanh@users.noreply.github.com>
2024-04-19 14:13:21 +0700

    Add files via upload

---
+++ BT1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wellcome!!!");
+        
+        while (true) {
+            System.out.print("Nhap mot so nguyen: ");
+            int number = scanner.nextInt();
+            
+            if (number % 2 == 0) {
+                System.out.println("So " + number + " la so chan.");
+            } else {
+                System.out.println("So " + number + " la so le.");
+            }
+            
+            System.out.print("Ban co muon tiep tuc? (Y/N): ");
+            String choice = scanner.next();
+            
+            if (!choice.equalsIgnoreCase("Y")) {
+                break;
+            }
+        }
+        
+        System.out.println("Ket thuc.");
+        scanner.close();
+    }
+}
